89de1229892475b77bec5a344bc9f09b72602b9b
saumya <2saumyaray@gmail.com>
2018-08-01 14:33:26 +0530

    wip: ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 
 import RootControllerView from './comps/RootControllerView.comp'
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Expense</h1>
-        </header>
-        <p className="App-intro">
-          Version 4. New features and bug fixes.
-        </p>
-        <RootControllerView />
-      </div>
-    );
+    return ( <RootControllerView /> );
   }
 }
 
