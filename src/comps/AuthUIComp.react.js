8d3267622e4d1217ee15833515787210bcac9aea
saumya <2saumyaray@gmail.com>
2018-09-19 16:09:15 +0530

    Google Login added

--- src/comps/AuthUIComp.react.js
+++ src/comps/AuthUIComp.react.js
@@ -4,6 +4,8 @@ import React, { Component, Fragment } from 'react'
 
 import Button from '@material-ui/core/Button';
 
+import * as firebase from 'firebase'
+
 
 
 class AuthUIComp extends Component {
@@ -35,7 +37,51 @@ class AuthUIComp extends Component {
 		console.log('AuthUIComp:onLoginWithGoogle');
 		var that = this;
 		var firebaseRef = this.props.firebaseRef;// 'this' refers to the class because of the 'bind()' we did in the 'constructor'
-	}
+		var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('profile');
+    provider.addScope('email');
+    // ref: https://firebase.google.com/docs/auth/web/google-signin?authuser=0 
+    //desktop : popup
+    firebaseRef.auth().signInWithPopup(provider).then(function(result) {
+    //mobile : redirect
+    //firebaseRef.auth().signInWithRedirect(provider).then(function(result) {
+      var token = result.credential.accessToken; //Google AccessToken
+      var user = result.user; //SignedIn User
+      /*
+      var database = firebase.database(); //DB Service
+      var currentUserDBPath = 'paid/'+result.user.uid; 
+      var databaseRef = firebase.database().ref(currentUserDBPath);
+      */
+      var gUID = result.user.uid;
+      //console.log('photourl',result.user.photoURL);
+      var gUser = { displayName : result.user.displayName,
+                    email : result.user.email,
+                    photoURL : result.user.photoURL
+                  }
+      that.setState({ isLoggedIn:true, googleUser:gUser });
+      //console.log('Google AccessToken',token);
+      //console.log('User',user);
+      that.props.loginSuccessCallback(token,gUser,gUID);
+    }).catch(function(error){
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+
+        console.group('Auth Error');
+        console.log('ERROR',error);
+        console.log('errorCode',errorCode);
+        console.log('errorMessage',errorMessage);
+        console.log('email',email);
+        console.log('credential',credential);
+        console.groupEnd();
+
+        that.props.errorCallback(errorMessage);
+    });
+	}// onLoginWithGoogle
 
 }// END Class
 
