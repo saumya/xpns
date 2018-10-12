644f9783e959cea0b0a052ee5ea8f3727f377e41
saumya <2saumyaray@gmail.com>
2018-10-12 15:48:27 +0530

    GSAP added to animate the About page logo

--- src/comps/AboutAppUIComp.react.js
+++ src/comps/AboutAppUIComp.react.js
@@ -11,18 +11,44 @@ import Button from '@material-ui/core/Button';
 
 import logo from '../logo.svg';
 
+import TweenMax from "gsap/TweenMax";
 
 
-const AboutAppUIComp = ({ children }) =>
-	<Fragment>
-		<center> 
-		  <header className="App-header-about" style={{marginTop:"3em"}}>
-		    <img src={logo} className="App-logo" alt="logo" width="200px" />
-		    <h1 className="App-title">Expense</h1>
-		    { children }
-		  </header>
-	  </center>
-  </Fragment>
 
+//const AboutAppUIComp = ({ children }) =>
+class AboutAppUIComp extends Component{
+	constructor(children){
+		super(children)
+		// reference to the DOM node
+	  this.myElement = document.getElementsByClassName("App-logo");;
+	  // reference to the animation
+	  //this.myTween = null;
+	}
+	componentDidMount(){
+		//this.myTween = TweenMax.to(this.myElement, 1, {x: 100, y: 100});
+		//this.myTween = TweenMax.to(this.myElement, 1, {rotation: 360, scale:1.5});
+		TweenMax.to(this.myElement, 2, { rotation:360, scale:1.5 });
+		TweenMax.to(this.myElement, 3, { delay:2, rotation:0, scale:1.0 });
+		TweenMax.to(this.myElement, 15, { delay:6, rotation:360, scale:1.5 });
+	}
+	render(){
+		return(
+			<Fragment>
+				<center> 
+				  <header className="App-header-about" style={{marginTop:"3em"}}>
+				    <img src={logo} className="App-logo" alt="logo" width="200px" />
+				    <h1 className="App-title">Expense</h1>
+				    { this.children }
+				  </header>
+			  </center>
+		  </Fragment>
+		)
+	}
+
+	
+  
+	
+	
+}
 
 export default AboutAppUIComp;
